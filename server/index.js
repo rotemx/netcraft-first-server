8e4c587aaee12446097cec60da1105f04791b0b9
Rotem Meiri <rotemx@users.noreply.github.com>
2023-05-16 18:53:53 +0300

    Initial commit - basic server and client

---
+++ server/index.js
@@ -0,0 +1,14 @@
+console.log("First Server Initializing...");
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+	res.send('<h1>Hello World!</h1>')
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+	
+})
