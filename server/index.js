c3c1e2d8797c5f803b04e6e6d017357a4b2bef46
Rotem Meiri <rotemx@users.noreply.github.com>
2023-05-16 20:12:23 +0300

    add post request

--- server/index.js
+++ server/index.js
@@ -1,14 +1,41 @@
-console.log("First Server Initializing...");
 
 const express = require('express')
 const app = express()
 const port = 3000
+const path          = require('path')
+const {application} = require("express");
 
-app.get('/', (req, res) => {
-	res.send('<h1>Hello World!</h1>')
+const products = [
+	{
+		name : "toy",
+		price: 23,
+		color: "red"
+	},
+	{
+		name : "tomato",
+		price: 2,
+		color: "red"
+	},
+	{
+		name : "laptop",
+		price: 2322,
+		color: "gray"
+	}
+]
+
+
+console.log("First Server Initializing...");
+console.log('running in ' + __dirname);
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.post('/products', (req, res) => {
+	console.log('received request');
+	res.json(products)
 })
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
-	
 })
+
+
